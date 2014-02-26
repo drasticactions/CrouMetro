7e64adf134d7f7797ddd92cf8bfcee6768003cbb
drasticactions <innerlogic4321@gmail.com>
2014-02-26 08:19:38 -0500

    Changes to Timeline item layout

---
+++ CuroMetro/Tools/ScreennameConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Croumetro.Tools
+{
+    public class ScreennameConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) return null;
+            return string.Format("@{0}", value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
